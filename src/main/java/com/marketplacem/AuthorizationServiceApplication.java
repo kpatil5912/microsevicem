393376d48f68a61662f3f872f48cd262dc3f97ac
komal patil <kpatil@csod.com>
2025-11-23 02:01:15 +0530

    removed apigateway keys from yml and added eureka client dependency

--- src/main/java/com/marketplacem/AuthorizationServiceApplication.java
+++ src/main/java/com/marketplacem/AuthorizationServiceApplication.java
@@ -3,10 +3,12 @@ package com.marketplacem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(exclude = {
 		OAuth2ClientAutoConfiguration.class
 })
+@EnableDiscoveryClient
 public class AuthorizationServiceApplication {
 
 	public static void main(String[] args) {
